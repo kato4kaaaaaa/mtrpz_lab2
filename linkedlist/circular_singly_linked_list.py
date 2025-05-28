2664adbc88947e42893bdb1baeb60b590bf6b8c7
Екатерина Гасюкова <ekaterinagasukova@MacBook-Air-Ekaterina.local>
2025-05-28 18:07:09 +0300

    Додано методи append та insert для кільцевого списку

--- linkedlist/circular_singly_linked_list.py
+++ linkedlist/circular_singly_linked_list.py
@@ -22,3 +22,26 @@ class CircularSinglyLinkedList:
             self.tail.next = new_node
             self.tail = new_node
         self._size += 1
+
+    def insert(self, element: str, index: int) -> None:
+        if index < 0 or index > self._size:
+            raise IndexError("Index out of bounds")
+
+        new_node = Node(element)
+        if self.tail is None:  # Порожній список
+            if index != 0:
+                raise IndexError("Index out of bounds")
+            new_node.next = new_node
+            self.tail = new_node
+        elif index == 0:  # Вставка в голову
+            new_node.next = self.tail.next
+            self.tail.next = new_node
+        else:
+            current = self.tail.next  # Починаємо з голови
+            for _ in range(index - 1):
+                current = current.next
+            new_node.next = current.next
+            current.next = new_node
+            if index == self._size:  # Вставка в кінець
+                self.tail = new_node
+        self._size += 1
