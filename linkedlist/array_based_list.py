b1abc3d01456245d12d1963baab8af15f5f6dff3
Екатерина Гасюкова <ekaterinagasukova@MacBook-Air-Ekaterina.local>
2025-05-28 19:57:53 +0300

    Додано методи append, insert та delete з перевіркою індексів

--- linkedlist/array_based_list.py
+++ linkedlist/array_based_list.py
@@ -4,3 +4,17 @@ class ArrayBasedList:
 
     def length(self) -> int:
         return len(self.data)
+
+    def append(self, element: str) -> None:
+        self.data.append(element)
+
+    def insert(self, element: str, index: int) -> None:
+        if index < 0 or index > len(self.data):
+            raise IndexError("Index out of bounds")
+        self.data.insert(index, element)
+
+    def delete(self, index: int) -> str:
+        if index < 0 or index >= len(self.data):
+            raise IndexError("Index out of bounds")
+        return self.data.pop(index)
+
