07b96090e113b41c9957d0d933371b8364baf7b6
Екатерина Гасюкова <ekaterinagasukova@MacBook-Air-Ekaterina.local>
2025-05-28 18:16:32 +0300

    Додано метод delete для кільцевого однобічно зв’язаного списку

--- linkedlist/circular_singly_linked_list.py
+++ linkedlist/circular_singly_linked_list.py
@@ -1,9 +1,3 @@
-class Node:
-    def __init__(self, data: str):
-        self.data = data
-        self.next = None
-
-
 class CircularSinglyLinkedList:
     def __init__(self):
         self.tail = None
@@ -28,20 +22,46 @@ class CircularSinglyLinkedList:
             raise IndexError("Index out of bounds")
 
         new_node = Node(element)
-        if self.tail is None:  # Порожній список
+
+        if self.tail is None:
             if index != 0:
                 raise IndexError("Index out of bounds")
             new_node.next = new_node
             self.tail = new_node
-        elif index == 0:  # Вставка в голову
+        elif index == 0:
             new_node.next = self.tail.next
             self.tail.next = new_node
         else:
-            current = self.tail.next  # Починаємо з голови
+            current = self.tail.next
             for _ in range(index - 1):
                 current = current.next
             new_node.next = current.next
             current.next = new_node
-            if index == self._size:  # Вставка в кінець
+            if index == self._size:
                 self.tail = new_node
+
         self._size += 1
+
+    def delete(self, index: int) -> str:
+        if self.tail is None or index < 0 or index >= self._size:
+            raise IndexError("Index out of bounds")
+
+        prev = self.tail
+        current = self.tail.next  # Починаємо з голови
+
+        for _ in range(index):
+            prev = current
+            current = current.next
+
+        # Видаляємо current
+        if current == self.tail:
+            if self._size == 1:
+                self.tail = None
+            else:
+                prev.next = current.next
+                self.tail = prev
+        else:
+            prev.next = current.next
+
+        self._size -= 1
+        return current.data
